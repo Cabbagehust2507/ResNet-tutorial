5c295e62e7637778af1e716ffebca3e3e5b435b5
iw <iwannatech-vunv@bitbucket.org>
2020-07-23 10:10:09 +0700

    add code dense block

---
+++ examples/dense_block_pytorch.py
@@ -0,0 +1,24 @@
+import torch 
+from torch import nn
+
+def conv_block(input_channels, num_channels):
+    return nn.Sequential(
+        nn.BatchNorm2d(input_channels), nn.ReLU(),
+        nn.Conv2d(input_channels, num_channels, kernel_size=3, padding=1)
+    )
+
+class DenseBlock(nn.Module):
+    def __init__(self, num_convs, input_channels, num_channels):
+        super(DenseBlock, self).__init__()
+        layer = []
+        for i in range(num_convs):
+            layer.append(conv_block(
+                num_channels*i + input_channels, num_channels
+            ))
+        self.net = nn.Sequential(*layer)
+    
+    def forward(self, X):
+        for blk in self.net:
+            Y = blk(X)
+            X = torch.cat((X, Y), dim=1)
+        return X
